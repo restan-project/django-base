5466638241a9fc9b024e30a809fdef9a107bda83
れぞ <monomono1963@gmail.com>
2020-01-25 23:42:35 +0900

    Revert "t"
    This reverts commit 2f4f2671c98dc94d32503c4de866f1b8b5e6a94f.

--- webtestapp/views.py
+++ webtestapp/views.py
@@ -1,8 +1,6 @@
 
 from django.shortcuts import render
 from webtestapp.forms import TestForm
-from firebase import firebase
-firebase = firebase.FirebaseApplication('https://rezo-sandbox.firebaseio.com', None)
 
 def index(request):
     my_dict = {
@@ -15,9 +13,6 @@ def index(request):
         my_dict['insert_forms'] = '文字列:' + request.POST['text'] + '<br>整数型:' + request.POST['num']
         my_dict['form'] = TestForm(request.POST)
 
-    result = firebase.post('/products', "{'id': '1'}, {'name': 'orange'}, {'price': '50'}")
-    result = firebase.post('/products', "{'id': '2'}, {'name': 'apple'}, {'price': '100'}")
-    result = firebase.post('/products', "{'id': '3'}, {'name': 'apple'}, {'price': '150'}")
     return render(request,'webtestapp/index.html',my_dict)
 
 
