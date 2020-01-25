582fe59021d96eea16ca964de42ccd20f6989150
れぞ <monomono1963@gmail.com>
2020-01-25 23:22:09 +0900

    Revert "test"
    This reverts commit 84c501bc2379ebbb8e5309e5041806ad1401a228.

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('', include('hello.urls')),  # 追加
-    path('', include('webtestapp.urls')),  # 追加
+    path('', include('hello.urls')),  # 追加
 ]
--- webtestapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name='webtestapp'
-
-urlpatterns = [
-    path('', views.index, name='index'),
-]
