b2a14d9212e8e6af55edf48d4c4d4166459f6388
れぞ <monomono1963@gmail.com>
2020-01-27 21:22:33 +0900

    tes

--- webtestapp/views.py
+++ webtestapp/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from webtestapp.forms import TestForm
 from firebase import firebase
 
+firebase = firebase.FirebaseApplication('https://rezo-sandbox.firebaseio.com', None)
 
 def index(request):
     my_dict = {
@@ -14,6 +15,11 @@ def index(request):
     if (request.method == 'POST'):
         my_dict['insert_forms'] = '文字列:' + request.POST['text'] + '<br>整数型:' + request.POST['num']
         my_dict['form'] = TestForm(request.POST)
+
+    result = firebase.post('/products', "{'id': '1'}, {'name': 'orange'}, {'price': '50'}")
+    result = firebase.post('/products', "{'id': '2'}, {'name': 'apple'}, {'price': '100'}")
+    result = firebase.post('/products', "{'id': '3'}, {'name': 'apple'}, {'price': '150'}")
+    result = firebase.get('/', None)
 
     return render(request,'webtestapp/index.html',my_dict)
 
