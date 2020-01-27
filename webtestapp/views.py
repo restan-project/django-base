c9dfed76889ec7514e62859165b4bb502c60fc07
れぞ <monomono1963@gmail.com>
2020-01-27 21:11:38 +0900

    te

--- webtestapp/views.py
+++ webtestapp/views.py
@@ -2,8 +2,7 @@
 from django.shortcuts import render
 from webtestapp.forms import TestForm
 from firebase import firebase
-#firebase = firebase.FirebaseApplication('https://rezo-sandbox.firebaseio.com', None)
-firebase = firebase.FirebaseApplication('https://rezo-sandbox.firebaseio.com', None)
+
 
 def index(request):
     my_dict = {
@@ -16,10 +15,11 @@ def index(request):
         my_dict['insert_forms'] = '文字列:' + request.POST['text'] + '<br>整数型:' + request.POST['num']
         my_dict['form'] = TestForm(request.POST)
 
-    result = firebase.post('/products', "{'id': '1'}, {'name': 'orange'}, {'price': '50'}")
-    result = firebase.post('/products', "{'id': '2'}, {'name': 'apple'}, {'price': '100'}")
-    result = firebase.post('/products', "{'id': '3'}, {'name': 'apple'}, {'price': '150'}")
-    result = firebase.get('/', None)
+    #firebase = firebasea.FirebaseApplication('https://rezo-sandbox.firebaseio.com', None)
+    #result = firebase.post('/products', "{'id': '1'}, {'name': 'orange'}, {'price': '50'}")
+    #result = firebase.post('/products', "{'id': '2'}, {'name': 'apple'}, {'price': '100'}")
+    #result = firebase.post('/products', "{'id': '3'}, {'name': 'apple'}, {'price': '150'}")
+    #result = firebase.get('/', None)
 
     return render(request,'webtestapp/index.html',my_dict)
 
