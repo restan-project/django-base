63306ae6050383b1f42ca53747adb01bed198404
TANANA <55340230+T47-tanana@users.noreply.github.com>
2020-02-08 21:02:10 +0900

    最新

--- webtestapp/views.py
+++ webtestapp/views.py
@@ -1,26 +1,13 @@
 
 from django.shortcuts import render
 from webtestapp.forms import TestForm
-from firebase import firebase
 
-firebase = firebase.FirebaseApplication('https://rezo-sandbox.firebaseio.com', None)
 
 def index(request):
     my_dict = {
     'insert_something':"views.pyのinsert_something部分です。",
     'name':'Bashi',
-    'form':TestForm(),
-    'insert_forms':'初期値',
     }
-    if (request.method == 'POST'):
-        my_dict['insert_forms'] = '文字列:' + request.POST['text'] + '<br>整数型:' + request.POST['num']
-        my_dict['form'] = TestForm(request.POST)
-
-    result = firebase.post('/products', "{'id': '1'}, {'name': 'orange'}, {'price': '50'}")
-    result = firebase.post('/products', "{'id': '2'}, {'name': 'apple'}, {'price': '100'}")
-    result = firebase.post('/products', "{'id': '3'}, {'name': 'apple'}, {'price': '150'}")
-    result = firebase.get('/', None)
-
     return render(request,'webtestapp/index.html',my_dict)
 
 
