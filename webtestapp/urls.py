19f7319dfa3a936f6ad60158de016c1a0203f04d
れぞ <monomono1963@gmail.com>
2020-02-08 22:09:54 +0900

    year

--- webtestapp/urls.py
+++ webtestapp/urls.py
@@ -5,4 +5,5 @@ app_name='webtestapp'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('api/', views.detail, name='detail'),
 ]
--- webtestapp/views.py
+++ webtestapp/views.py
@@ -1,6 +1,7 @@
 
 from django.shortcuts import render
 from webtestapp.forms import TestForm
+from django.http import HttpResponse
 
 
 def index(request):
@@ -9,5 +10,15 @@ def index(request):
     'name':'Bashi',
     }
     return render(request,'webtestapp/index.html',my_dict)
+
+def api_test(request):
+    my_dict = {
+    'insert_something':"views.pyのinsert_something部分です。",
+    'name':'name',
+    }
+    return render(request,'webtestapp/index.html',my_dict)
+
+def detail(request):
+    return HttpResponse("You're looking at question %s.")
 
 
